f4301eae1e2bfdb24525e5963497ae12f932ac29
DESKTOP-BNCF92P\admin <3032848403@qq.com>
2026-09-08 19:13:42 +0800

    停服公告

---
+++ assets/ButtonDropdown.vue_vue_type_script_setup_true_lang-C6wBzPuC.js
@@ -0,0 +1,32 @@
+import{s as l,b6 as g,b8 as n,r,b7 as p,t as $,u as x,x as C,cR as N,cW as z,cF as R,el as S,I as T,H as c,a4 as h,a5 as u,W as D,ad as L,U as P,P as W,a7 as E,Q as G,N as b}from"./index-Ct9cQUdc.js";import{N as I}from"./Dropdown-nKEwravd.js";const t="0!important",_="-1px!important";function i(o){return n(`${o}-type`,[r("& +",[l("button",{},[n(`${o}-type`,[p("border",{borderLeftWidth:t}),p("state-border",{left:_})])])])])}function s(o){return n(`${o}-type`,[r("& +",[l("button",[n(`${o}-type`,[p("border",{borderTopWidth:t}),p("state-border",{top:_})])])])])}const j=l("button-group",`
+ flex-wrap: nowrap;
+ display: inline-flex;
+ position: relative;
+`,[g("vertical",{flexDirection:"row"},[g("rtl",[l("button",[r("&:first-child:not(:last-child)",`
+ margin-right: ${t};
+ border-top-right-radius: ${t};
+ border-bottom-right-radius: ${t};
+ `),r("&:last-child:not(:first-child)",`
+ margin-left: ${t};
+ border-top-left-radius: ${t};
+ border-bottom-left-radius: ${t};
+ `),r("&:not(:first-child):not(:last-child)",`
+ margin-left: ${t};
+ margin-right: ${t};
+ border-radius: ${t};
+ `),i("default"),n("ghost",[i("primary"),i("info"),i("success"),i("warning"),i("error")])])])]),n("vertical",{flexDirection:"column"},[l("button",[r("&:first-child:not(:last-child)",`
+ margin-bottom: ${t};
+ margin-left: ${t};
+ margin-right: ${t};
+ border-bottom-left-radius: ${t};
+ border-bottom-right-radius: ${t};
+ `),r("&:last-child:not(:first-child)",`
+ margin-top: ${t};
+ margin-left: ${t};
+ margin-right: ${t};
+ border-top-left-radius: ${t};
+ border-top-right-radius: ${t};
+ `),r("&:not(:first-child):not(:last-child)",`
+ margin: ${t};
+ border-radius: ${t};
+ `),s("default"),n("ghost",[s("primary"),s("info"),s("success"),s("warning"),s("error")])])])]),M={size:{type:String,default:void 0},vertical:Boolean},V=$({name:"ButtonGroup",props:M,setup(o){const{mergedClsPrefixRef:a,mergedRtlRef:d}=C(o);return N("-button-group",j,a),R(S,o),{rtlEnabled:z("ButtonGroup",d,a),mergedClsPrefix:a}},render(){const{mergedClsPrefix:o}=this;return x("div",{class:[`${o}-button-group`,this.rtlEnabled&&`${o}-button-group--rtl`,this.vertical&&`${o}-button-group--vertical`],role:"group"},this.$slots)}}),y=T("down",!1,function(o){return c("svg",{width:o.size,height:o.size,viewBox:"0 0 48 48",fill:"none"},[c("path",{d:"M36 18L24 30L12 18",stroke:o.colors[0],"stroke-width":o.strokeWidth,"stroke-linecap":o.strokeLinecap,"stroke-linejoin":o.strokeLinejoin},null)])}),F={components:{Down:y}},U=$({...F,__name:"ButtonDropdown",props:{primaryText:{default:"操作"},primaryType:{default:"primary"},options:{default:()=>[]},size:{default:"medium"},ghost:{type:Boolean,default:!1}},emits:["primary-click","select"],setup(o,{emit:a}){const d=a,m=()=>{d("primary-click")},w=e=>{d("select",e)};return(e,H)=>{const f=L,k=E,B=I,v=V;return b(),h(v,{size:"tiny"},{default:u(()=>[c(f,{type:e.primaryType,ghost:e.ghost,onClick:m},{default:u(()=>[P(W(e.primaryText),1)]),_:1},8,["type","ghost"]),e.options.length>0?(b(),h(B,{key:0,placement:"bottom-end",trigger:"click",size:"small","show-arrow":!0,onSelect:w,options:e.options},{default:u(()=>[c(f,{ghost:e.ghost},{icon:u(()=>[c(k,{component:G(y)},null,8,["component"])]),_:1},8,["ghost"])]),_:1},8,["options"])):D("",!0)]),_:1})}}});export{U as _};
